2747122e584c53c6083abbd7e44fb4b49c71a4a3
Sirawan Wankham <67219010021@technicrayong.ac.th>
2026-04-26 16:18:49 +0700

    jssit

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 //
 // Variables
 //
+const list          = document.getElementById("list");
+const showGPAResult = document.getElementById("show-gpa-result");
+const showHistory   = document.getElementById("show-history");
+const buttonAdd     = document.getElementById("buttonAdd");
+const buttonReset   = document.getElementById("buttonReset");
+const buttonCal     = document.getElementById("buttonCal");
 
-
+console.log(list)
 //
 // Methods
 //
+function handleAdd(){
+    console.log("handle Add")
+}
 
+function handleReset() {
+    console.log("handle Reset")
+}
 
+function handleCal() {
+    console.log("handle Cal")
+}
 //
 // Inits & Event Listeners
 //
+
+buttonAdd.addEventListener("click", handleAdd)
+buttonReset.addEventListener("click", handleReset)
+buttonCal.addEventListener("click", handleCal)
